ed7ea8a481b36d8a6b20e2d067ddb87da65e813b
Vlad <vladislav.bok@yandex.ru>
2021-08-11 18:13:27 +0300

    Initial Commit

---
+++ HW6_AudioPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW6_AudioPlayer
+//
+//  Created by Влад Бокин on 11.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
