cfdcdad74acd171f2565e5a618af949ba81d442a
Vlad <vladislav.bok@yandex.ru>
2021-08-18 21:56:13 +0300

    Сделал время воспроизведения трека и кнопку поделиться

---
+++ HW6_AudioPlayer/FirstViewController.swift
@@ -0,0 +1,154 @@
+//
+//  FirstViewController.swift
+//  HW6_AudioPlayer
+//
+//  Created by Влад Бокин on 11.08.2021.
+//
+
+import UIKit
+import AVFoundation
+
+class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var playPauseButtonOutlet: UIButton!
+    @IBOutlet weak var currentPlayerValueLabel: UILabel!
+    @IBOutlet weak var songImage: UIImageView!
+    @IBOutlet weak var playerDurationLabel: UILabel!
+    
+    var timer: Timer?
+    var activitiViewContorller: UIActivityViewController? = nil
+    var player = AVAudioPlayer()
+    let durationSlider = UISlider()
+    let soundSlider = UISlider()
+    var songName: String = "The_Weeknd_Blinding_Lights"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //MARK: - songImage
+        songImage.layer.shadowRadius = 20
+        songImage.layer.shadowOffset = .zero
+        songImage.layer.shadowOpacity = 0.5
+        songImage.layer.shadowColor = UIColor.black.cgColor
+        songImage.layer.shadowPath = UIBezierPath(rect: songImage.bounds).cgPath
+        songImage.layer.masksToBounds = false
+        songImage.layer.shadowOffset = CGSize(width: 0, height: 10)
+        
+        
+        //MARK: - VolumeSlider
+        
+        soundSlider.frame = CGRect(x: 40, y: self.view.frame.height - 200, width: self.view.frame.width - 80, height: 20)
+        soundSlider.minimumTrackTintColor = #colorLiteral(red: 0.4988975525, green: 0.4993980527, blue: 0.5142464638, alpha: 1)
+        self.view.addSubview(soundSlider)
+        soundSlider.minimumValue = 0.0
+        soundSlider.maximumValue = 100.0
+        soundSlider.value = 70.0
+        
+        //MARK: - DurationSlider
+        durationSlider.frame = CGRect(x: 40, y: self.view.frame.height - 330, width: self.view.frame.width - 80, height: 20)
+        durationSlider.minimumTrackTintColor = #colorLiteral(red: 0.9949335456, green: 0.2728733718, blue: 0.4223648906, alpha: 1)
+        self.view.addSubview(durationSlider)
+        durationSlider.minimumValue = 0.0
+        durationSlider.maximumValue = 100.0
+        
+        self.playPauseButtonOutlet.setImage(UIImage(named: "pause"), for: .normal)
+        
+        //MARK: - Slider addTarget
+        self.durationSlider.addTarget(self, action: #selector(changeSlider), for: .valueChanged )
+        self.soundSlider.addTarget(self, action: #selector(volumeSlider), for: .valueChanged)
+        
+        //MARK: - create audioPath
+        do {
+            if let audioPath = Bundle.main.path(forResource: "The_Weeknd_Blinding_Lights", ofType: "mp3"){
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath))
+            self.durationSlider.maximumValue = Float(player.duration)
+            self.soundSlider.maximumValue = Float(player.volume)
+            }
+        } catch {
+            print("Error")
+        }
+        self.player.play()
+        
+        //MARK: - timer from extention
+        createTimer()
+    }
+    
+    
+    @objc func changeSlider(sender: UISlider) {
+        player.stop()
+        player.currentTime = TimeInterval(durationSlider.value)
+                if isPlaying {
+                    player.prepareToPlay()
+                    player.play()
+                }
+    }
+    
+    func configuratePlayerDurationLabel() {
+            var minutes = Int(player.duration / 60)
+            let seconds = player.duration - Double(minutes * 60)
+            var secodsString = seconds < 9.5 ? "0\(String(format: "%.0f", seconds))" : "\(String(format: "%.0f", seconds))"
+            if secodsString == "60" {
+                secodsString = "00"
+                minutes += 1
+            }
+            playerDurationLabel.text = "\(minutes):\(secodsString)"
+        }
+    
+    
+    @objc func volumeSlider(sender: UISlider) {
+        if sender == soundSlider{
+            self.player.volume = self.soundSlider.value
+        }
+    }
+    var isPlaying = true
+    
+    //MARK: - Buttons
+    @IBAction func playPauseButton(_ sender: Any) {
+        if isPlaying == true{
+            DispatchQueue.main.async {
+                self.playPauseButtonOutlet.setImage(UIImage(named: "play"), for: .normal)
+                self.player.stop()
+            }
+            isPlaying = false
+            
+        } else {
+            DispatchQueue.main.async {
+                self.playPauseButtonOutlet.setImage(UIImage(named: "pause"), for: .normal)
+                self.player.play()
+            }
+            isPlaying = true
+            }
+    }
+        
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func shareButton(_ sender: Any) {
+        self.activitiViewContorller = UIActivityViewController(activityItems: [self.songName ], applicationActivities: nil)
+        self.present(self.activitiViewContorller!, animated: true, completion: nil)
+    }
+    
+
+}
+extension FirstViewController {
+    @objc func updateTimer() {
+        durationSlider.value = Float(player.currentTime)
+        var minutes = Int(player.currentTime / 60)
+        let seconds = player.currentTime - Double(minutes * 60)
+        var secodsString = seconds < 9.5 ? "0\(String(format: "%.0f", seconds))" : "\(String(format: "%.0f", seconds))"
+        if secodsString == "60" {
+            secodsString = "00"
+            minutes += 1
+        }
+        currentPlayerValueLabel.text = "\(minutes):\(secodsString)"
+    }
+    
+    func createTimer() {
+        if timer == nil {
+            timer = Timer.scheduledTimer(withTimeInterval: 1/5, repeats: true) { [weak self]_ in
+                self?.updateTimer()
+            }
+        }
+    }
+}
--- HW6_AudioPlayer/ViewController.swift
+++ HW6_AudioPlayer/ViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @objc func buttonAction(sender: UIButton!) {
+        self.performSegue(withIdentifier: "FirstViewController", sender: "John")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "showVC" {
+                if let vc = segue.destination as? FirstViewController {
+                    guard let text = sender as? String else { return }
+                }
+            }
+        }
+    
+    var artistName: String!
+    var songName: [String : String] = ["Weekend" : "Bliding light", "Michael Kiwanuka" : "Cold little heart"]
+    
+//    var songName: [String] = ["Weekend - Bliding light", "Michael Kiwanuka - Cold little heart"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var i = 1
+        func createCell(){
+            for (name, artist) in songName {
+            let areaButton = UIButton()
+                areaButton.frame = CGRect(x: 0, y: 100 * i , width: Int(self.view.frame.width), height: 100)
+   
+            
+            areaButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+            self.view.addSubview(areaButton)
+
+            let musicLabel = UILabel()
+                musicLabel.frame = CGRect(x: Int(self.view.frame.width) / 2 - 50, y: 50, width: 250, height: 30)
+            musicLabel.font = UIFont(name:"HelveticaNeue", size: 20.0)
+            musicLabel.text = "Music player"
+            self.view.addSubview(musicLabel)
+            
+            let cellView = UIView()
+                cellView.frame = CGRect(x: 20, y: 200, width: Int(self.view.frame.width) - 40, height: 1)
+            cellView.backgroundColor = UIColor.lightGray
+            self.view.addSubview(cellView)
+            
+            let nameLabel = UILabel()
+            nameLabel.frame = CGRect(x: 110, y: 105 * i, width: 250, height: 50)
+                nameLabel.text = "\(name)"
+                nameLabel.font = UIFont(name:"HelveticaNeue-Bold", size: 20.0)
+                self.view.addSubview(nameLabel)
+            
+            let songLabel = UILabel()
+            songLabel.frame = CGRect(x: 110, y: 125 * i, width: 250, height: 50)
+                songLabel.text = "\(artist)"
+            songLabel.font = UIFont(name:"HelveticaNeue-Bold", size: 20.0)
+                self.view.addSubview(songLabel)
+            
+            let songImage = UIImageView()
+            songImage.frame = CGRect(x: 10, y: 105 * i , width: 90, height: 90) //song count
+            songImage.image = UIImage(named: "\(name)")
+            self.view.addSubview(songImage)
+            i += 1
+            }
+        }
+        createCell()
+           
+        
     }
 
+ 
+    
 
 }
 
